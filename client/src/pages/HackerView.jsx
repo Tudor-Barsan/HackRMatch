b31d4e82216c76381412e1a1e99184683a34685d
Jessica <jessica2003mail@gmail.com>
2023-08-19 17:30:19 -0400

    progress in displaying hackers

--- client/src/pages/HackerView.jsx
+++ client/src/pages/HackerView.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const HackerView = () => {
+  const [matches, setMatches] = useState(null)
+
+  useEffect(() => {
+    const fetchMatches = async () => {
+      const response = await fetch('/api', {
+        headers: {'Content-Type': 'application/json'},
+      })
+      const json = await response.json()
+      if (response.ok) {
+        setMatches(json)
+      }
+    }
+
+    fetchMatches()
+  }, [])
     
   return <div>HackerView</div>;
 };

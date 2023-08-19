ad37434080e649e1958f138f3ea33ac49ebe1554
daniel-su1 <107098486+daniel-su1@users.noreply.github.com>
2023-08-19 02:17:59 -0400

    add fake users that go to mongodb

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,6 @@
 // models / userModel.js
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema
+import mongoose from 'mongoose'
+import { Schema } from 'mongoose'
 
 const userSchema = new Schema({
   // about me
@@ -72,6 +72,4 @@ const userSchema = new Schema({
   ],
 });
 
-const User = mongoose.model('User', userSchema);
-
-module.exports = User;
+export const User = mongoose.model("User", userSchema);

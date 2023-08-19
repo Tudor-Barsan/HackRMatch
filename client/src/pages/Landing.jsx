ae9fa97bae06727c35b29cc5897efaa7a70bfdc5
Tudor <tudor.barsan46@gmail.com>
2023-08-19 17:17:15 -0400

    Create profile page

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 
 const Landing = () => {
     return (
-      <body>
+      <body style={{alignItems: "center"}}>
         <img
           className="w-1/2 max-w-sm mx-auto"
           src="../HackRMatchLogo.png"
           alt="HackRMatch Logo"
         ></img>
-          <Link to="/create-profile">
+          <Link to="/create-profile" className="px-8">
             Sign in
           </Link>
       </body>

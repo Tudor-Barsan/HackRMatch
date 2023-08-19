a77532fcded5c047955f37db82604170ba3ed14e
daniel-su1 <107098486+daniel-su1@users.noreply.github.com>
2023-08-19 13:00:41 -0400

    add user validation

--- server/middleware/middleware.js
+++ server/middleware/middleware.js
@@ -1,2 +1,14 @@
 import User from "../models/user.js";
+import userSchema from "../schemas.js";
+import ExpressError from "../utils/expressError.js";
+
+ export const validateUser = (req, res, next) => {
+  const { error } = userSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
 

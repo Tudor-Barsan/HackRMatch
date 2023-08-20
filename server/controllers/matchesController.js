94dcd488b80ad2ce1484ed479b54798826f7667f
evelina <91142355+135ze@users.noreply.github.com>
2023-08-19 21:55:47 -0400

    update seed

--- server/controllers/matchesController.js
+++ server/controllers/matchesController.js
@@ -1,7 +1,8 @@
 import User from '../models/user.js'
 
 export const findMatches = async (req, res) => {
-    const userId = '64e10d62ae335f52164b0c9f'; //getUserId(req)
+    const userId = '64e1707f7225d1733daeb6cf'; //getUserId(req)
+    console.log(User.find())
     const { possibleMatches } = await User.findById(userId).populate('possibleMatches'); 
 
     await res.status(200).json(possibleMatches);

eff36db1e0362451918a63635943b9d894e222dc
evelina <91142355+135ze@users.noreply.github.com>
2023-08-20 01:27:52 -0400

    update fake users again

--- server/controllers/matchesController.js
+++ server/controllers/matchesController.js
@@ -1,9 +1,8 @@
 import User from '../models/user.js'
 
 export const findMatches = async (req, res) => {
-    const userId = '64e1707f7225d1733daeb6cf'; //getUserId(req)
-    const { possibleMatches } = await User.findById(userId).populate('possibleMatches'); 
-
+    const userId = '64e1a32135e95418d5188446'; //getUserId(req)
+    const { possibleMatches } = await User.findById(userId).populate('possibleMatches');
     await res.status(200).json(possibleMatches);
-    
+
 }

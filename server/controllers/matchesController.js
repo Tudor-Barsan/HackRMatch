aa57aab168af0b04d0b61bd8bbdd0c340a3efdba
evelina <91142355+135ze@users.noreply.github.com>
2023-08-20 02:16:29 -0400

    fix navbar home logo

--- server/controllers/matchesController.js
+++ server/controllers/matchesController.js
@@ -1,7 +1,7 @@
 import User from '../models/user.js'
 
 export const findMatches = async (req, res) => {
-    const userId = '64e1a32135e95418d5188446'; //getUserId(req)
+    const userId = '64e1a4aca22e0d2170d3b8e5'; //getUserId(req)
     const { possibleMatches } = await User.findById(userId).populate('possibleMatches');
     await res.status(200).json(possibleMatches);
 
